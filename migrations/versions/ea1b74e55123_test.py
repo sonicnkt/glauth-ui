c193864a0e5630bcfd07daa83ce30f215816fc00
sonicnkt <da.schizzle@gmail.com>
2021-12-22 12:51:55 +0100

    revert to old migration file

--- migrations/versions/ea1b74e55123_test.py
+++ migrations/versions/ea1b74e55123_test.py
@@ -21,11 +21,11 @@ def upgrade():
     op.create_table('group',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=20), nullable=False),
-    sa.Column('gidnumber', sa.Integer(), nullable=False),
+    sa.Column('unixid', sa.Integer(), nullable=False),
     sa.Column('primary', sa.Boolean(), nullable=False),
     sa.Column('description', sa.String(length=255), nullable=True),
     sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('gidnumber')
+    sa.UniqueConstraint('unixid')
     )
     op.create_index(op.f('ix_group_name'), 'group', ['name'], unique=True)
     op.create_table('settings',
@@ -43,8 +43,8 @@ def upgrade():
     op.create_table('included_groups',
     sa.Column('include_id', sa.Integer(), nullable=True),
     sa.Column('included_in_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['include_id'], ['group.gidnumber'], ),
-    sa.ForeignKeyConstraint(['included_in_id'], ['group.gidnumber'], )
+    sa.ForeignKeyConstraint(['include_id'], ['group.unixid'], ),
+    sa.ForeignKeyConstraint(['included_in_id'], ['group.unixid'], )
     )
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
@@ -52,21 +52,21 @@ def upgrade():
     sa.Column('mail', sa.String(length=50), nullable=True),
     sa.Column('givenname', sa.String(length=40), nullable=True),
     sa.Column('surname', sa.String(length=40), nullable=True),
-    sa.Column('uidnumber', sa.Integer(), nullable=False),
+    sa.Column('unixid', sa.Integer(), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=False),
     sa.Column('password_hash', sa.String(length=64), nullable=False),
     sa.Column('primarygroup', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['primarygroup'], ['group.gidnumber'], ),
+    sa.ForeignKeyConstraint(['primarygroup'], ['group.unixid'], ),
     sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('uidnumber')
+    sa.UniqueConstraint('unixid')
     )
     op.create_index(op.f('ix_user_mail'), 'user', ['mail'], unique=True)
     op.create_index(op.f('ix_user_username'), 'user', ['username'], unique=True)
     op.create_table('othergroups_users',
     sa.Column('user_id', sa.Integer(), nullable=True),
     sa.Column('group_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['group_id'], ['group.gidnumber'], ),
-    sa.ForeignKeyConstraint(['user_id'], ['user.gidnumber'], )
+    sa.ForeignKeyConstraint(['group_id'], ['group.unixid'], ),
+    sa.ForeignKeyConstraint(['user_id'], ['user.unixid'], )
     )
     # ### end Alembic commands ###
 
