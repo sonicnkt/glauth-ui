f5b2e57f9026328867d70dc8d9523935c7c9de6a
threadnought <103188322+threadnought@users.noreply.github.com>
2022-04-07 11:23:18 -0400

    cleanup config parsing

--- config.py
+++ config.py
@@ -1,28 +1,34 @@
-import os
+from ast import literal_eval
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    APPNAME = os.environ.get('APPNAME') or 'Glauth UI'
-    ORGANISATION = os.environ.get('ORGANISATION') or 'Glauth UI - Team'
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    ADMIN_GROUP = os.environ.get('ADMIN_GROUP') or 'glauth_admin'
+    def parse_env(key, default, _type=str):
+        if _type == str:
+            return os.environ.get(key, default)
+        return _type(literal_eval(os.environ.get(key, default)))
+
+    APPNAME      = parse_env('APPNAME', 'Glauth UI')
+    ORGANISATION = parse_env('ORGANISATION', 'Glauth UI - Team')
+    SECRET_KEY   = parse_env('SECRET_KEY', 'you-will-never-guess')
+    ADMIN_GROUP  = parse_env('ADMIN_GROUP', 'glauth_admin')
 
     # MAIL Config
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_ADMIN = os.environ.get('MAIL_ADMIN') or 'admin@example.com'
+    MAIL_SERVER   = parse_env('MAIL_SERVER')
+    MAIL_PORT     = parse_env('MAIL_PORT', '25', int)
+    MAIL_USE_TLS  = parse_env('MAIL_USE_TLS', 'False', bool)
+    MAIL_USERNAME = parse_env('MAIL_USERNAME')
+    MAIL_PASSWORD = parse_env('MAIL_PASSWORD')
+    MAIL_ADMIN    = parse_env('MAIL_ADMIN', 'admin@example.com')
     # Gets Errors and is sender of emails send to users
 
     # Database Stuff
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = parse_env('DATABASE_URL', \
         'sqlite:///' + os.path.join(basedir, 'db', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     # Glauth Stuff 
-    GLAUTH_CFG_PATH = os.environ.get('GLAUTH_CFG_PATH') or \
+    GLAUTH_CFG_PATH = parse_env('GLAUTH_CFG_PATH', \
         os.path.join(basedir, 'db', 'config.cfg')
     
     # FLASK ADMIN STUFF
-    FLASK_ADMIN_FLUID_LAYOUT = False
+    FLASK_ADMIN_FLUID_LAYOUT = parse_env('FLASK_ADMIN_FLUID_LAYOUT', 'False', bool)
